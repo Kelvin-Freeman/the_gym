9d03c0e27a3256c5ecb8c34e9d0c87602705bb94
Kelvin Freeman <107550014+Kelvin-Freeman@users.noreply.github.com>
2022-10-07 14:19:44 -0400

    this is a test

---
+++ gym.js
@@ -0,0 +1,14 @@
+
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World!</h1>')
+})
+
+
+
+app.listen(3000, function() {
+    console.log(`Server listening in port: 3000`)
+})
